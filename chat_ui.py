5a18a0bf87ee9b8c84bc68b49e1d947284ec4558
xnileshtiwari <135645478+xnileshtiwari@users.noreply.github.com>
2025-03-13 01:58:45 +0530

    Update chat_ui.py

--- chat_ui.py
+++ chat_ui.py
@@ -233,7 +233,7 @@ pdf_documents = [
 # Function to get available CSV documents
 def get_csv_documents():
     # Change this path to where your CSV documents are stored
-    csv_path = "/home/xnileshtiwari/vscode/new-gemini-upload-file/DATABASE2/"
+    csv_path = "all_csv_documents/"
     data_files = []
     for file in os.listdir(csv_path):
         if file.endswith('.csv') or file.endswith('.xlsx'):
