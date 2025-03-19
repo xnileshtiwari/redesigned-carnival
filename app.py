a96e47820d768c6d446df800d326cf083adb1404
xnileshtiwari <135645478+xnileshtiwari@users.noreply.github.com>
2025-03-19 16:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ pdf_documents = [
 # Function to get available CSV documents
 def get_csv_documents():
     # Change this path to where your CSV documents are stored
-    csv_path = "redesigned-carnival/Updated_CSVs/"
+    csv_path = "all_csv_documents/"
     data_files = []
     for file in os.listdir(csv_path):
         if file.endswith('.csv') or file.endswith('.xlsx'):
